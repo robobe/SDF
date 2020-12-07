c4bfe36827af6ae152e354015ab82175126e2c8b
robo <robo>
2020-12-07 12:18:49 +0200

    run on save
    add output log

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,29 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	let sdf_log = vscode.window.createOutputChannel("SDF");
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
+	sdf_log.appendLine("hello");
 	console.log('Congratulations, your extension "sdf" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sdf.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('sdf.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from XXX!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from XXX!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
+	
+		vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument)=>{
+			vscode.window.setStatusBarMessage('doc save');
+			vscode.window.showInformationMessage(document.getText());
+		}
+	);
 }
 
 // this method is called when your extension is deactivated
