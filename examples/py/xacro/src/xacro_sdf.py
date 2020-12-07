529a9c23b922dd6a914b492f4da42362f854cec1
robo <robo>
2020-12-07 20:54:15 +0200

    xacro2sdf v0.1

--- examples/py/xacro/src/xacro_sdf.py
+++ examples/py/xacro/src/xacro_sdf.py
@@ -1,5 +1,6 @@
 import os
 import re
+from types import FunctionType
 
 from xml.dom import minidom
 
@@ -10,69 +11,118 @@ def try_convert_to_number(value):
         return value
 
 class converter():
-    def __init__(self, input_file):
+    def __init__(self):
+        self.__root_doc = None
         self.__doc = None
         self.__properties = {}
-        self.__load(input_file)
+        self.__macros = {}
+        self.__file_dir = ""
 
-    def __load(self, file_name):
-        self.__doc = minidom.parse(file_name).documentElement
-        self.__parse_properties()
+    def run(self, input_file):
+        self.__file_dir = os.path.dirname(input_file)
+        self.__load(input_file, root=True)
         
         self.__save()
-        # print(dir(self.__doc))
 
-    def __parse_properties(self):
-        elements = self.__doc.getElementsByTagNameNS("http://dd", "property")
-        for node in elements:
-            name = node.getAttribute("name")
-            value = node.getAttribute("value")
-            self.__properties[name] = try_convert_to_number(value)
-            parent = node.parentNode
-            parent.removeChild(node)
-
-        self.__subsitute()
-
-    def __sub_txt(self, obj):
-        key = obj.group(1)
-        data = eval(key, self.__properties)
-        return str(data)
+    def __load(self, file_name, root=False):
+        doc = minidom.parse(file_name).documentElement
+        if root:
+            self.__root_doc = doc
+        self.__load_includes(doc)
+        self.__run(doc)
         
 
-    def __eval_text(self, value):
-        pattern = re.compile(r'[$][{](.*?)[}]', re.S)
-        new_value = re.sub(pattern, self.__sub_txt, value)
-        return new_value!=value, new_value
-
-    def __recursively(self, node):
+    def __run(self, node):
         for e in node.childNodes:
             if e.nodeType == minidom.Node.ELEMENT_NODE:
-                for k, v in e.attributes.items():
-                    replace , value = self.__eval_text(v)
-                    if replace:
-                        e.setAttribute(k, value)
+                if "xacro:" in e.tagName:
+                    cmd = e.tagName[6:]
+                    if cmd == "macro":
+                        self.__load_macros(e)
+                    elif cmd == "property":
+                        self.__load_properties(e)
+                    else:
+                        self.__run_macro(e, cmd)
+                else:
+                    for k, v in e.attributes.items():
+                        replace , value = self.__eval_text(v)
+                        if replace:
+                            e.setAttribute(k, value)
             if e.nodeType == minidom.Node.TEXT_NODE:
                 if len(e.nodeValue.strip()) > 0:
                     replace , value = self.__eval_text(e.nodeValue)
                     e.nodeValue = value
-            self.__recursively(e)
 
-    def __subsitute(self):
-        self.__recursively(self.__doc)
+            if e.nodeType == minidom.Node.ELEMENT_NODE:
+                if "xacro:" not in e.tagName:
+                    self.__run(e)
 
+    def __run_macro(self, node, cmd):
+        kwargs = {}
+        for k,v in node.attributes.items():
+            kwargs[k] = v
+        data = self.__macros[cmd](**kwargs)
+        _, data = self.__eval_text(data, kwargs)
+        parent = node.parentNode
+        parent.removeChild(node)
+        inner = minidom.parseString(data).documentElement
+        parent.appendChild(inner)
+
+    def __load_macros(self, node):
+        name = node.getAttribute("name")
+        params = node.getAttribute("params")
+        for e in node.childNodes:
+            if e.nodeType == minidom.Node.ELEMENT_NODE:
+                break
+        params_list = (",".join(params.strip().split()))
+        f_body = e.toxml()
+        f_func_string = f'def {name}({params_list}): return """{f_body}"""'
+        f_code = compile(f_func_string, "<string>", "exec")
+        f_func = FunctionType(f_code.co_consts[0], globals(), name)
+        self.__macros[name] = f_func
+        parent = node.parentNode
+        parent.removeChild(node)
+
+    def __load_includes(self, node):
+        elements = node.getElementsByTagNameNS("http://dd", "include")
+        for node in elements:
+            if not node.hasAttribute("uri"):
+                raise Exception("include uri not declare")
+            pattern = re.compile(r'file://(.*?)', re.S)
+            new_value = re.sub(pattern, lambda x: x.group(1), node.getAttribute("uri"))
+            
+            file_name = os.path.join(self.__file_dir, new_value)
+            parent = node.parentNode
+            parent.removeChild(node)
+            self.__load(file_name)
+
+    def __load_properties(self, node):
+        name = node.getAttribute("name")
+        value = node.getAttribute("value")
+        self.__properties[name] = try_convert_to_number(value)
+        parent = node.parentNode
+        parent.removeChild(node)
+        
+    def __eval_text(self, value, kwargs={}):
+        subsitute = lambda o: str(eval(o.group(1), self.__properties, kwargs))
+        pattern = re.compile(r'[$][{](.*?)[}]', re.S)
+        new_value = re.sub(pattern, subsitute, value)
+        return new_value!=value, new_value
 
     def __save(self):
-        dir = os.path.dirname(__file__)
-        file_name = os.path.join(dir, "../dest_sdf")
+        dir_name = os.path.dirname(__file__)
+        file_name = os.path.join(dir_name, "../dest_sdf")
         file_name = os.path.join(file_name, "property.sdf")
         with (open(file_name, "w")) as f:
-            self.__doc.writexml(f)
+            data = self.__root_doc.toxml()
+            f.writelines(data)
 
 def main():
-    dir = os.path.dirname(__file__)
-    file_name = os.path.join(dir, "../source_example")
-    file_name = os.path.join(file_name, "property.xacro")
-    con = converter(file_name)
+    dir_name = os.path.dirname(__file__)
+    file_name = os.path.join(dir_name, "../source_example")
+    file_name = os.path.join(file_name, "macro.xacro")
+    con = converter()
+    con.run(file_name)
 
 if __name__ == "__main__":
     main()
