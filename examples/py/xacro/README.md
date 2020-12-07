87840d2fee872ecca5d603d03ff3e5a1ad5bc334
robo <robo>
2020-12-07 15:35:42 +0200

    python xarco

---
+++ examples/py/xacro/README.md
@@ -0,0 +1,8 @@
+# XACRO
+
+## property
+```
+<xacro:property name="width" value=".2" />
+
+<cylinder radius="${width}" length="${bodylen}"/>
+```
---
+++ examples/py/xacro/src/xacro_sdf.py
@@ -0,0 +1,71 @@
+import os
+import re
+
+from xml.dom import minidom
+
+
+
+class converter():
+    def __init__(self, input_file):
+        self.__doc = None
+        self.__properties = {}
+        self.__load(input_file)
+
+    def __load(self, file_name):
+        self.__doc = minidom.parse(file_name).documentElement
+        self.__parse_properties()
+        
+        self.__save()
+        # print(dir(self.__doc))
+
+    def __parse_properties(self):
+        elements = self.__doc.getElementsByTagNameNS("http://dd", "property")
+        for node in elements:
+            name = node.getAttribute("name")
+            value = node.getAttribute("value")
+            self.__properties[name] = value
+            parent = node.parentNode
+            parent.removeChild(node)
+
+        self.__subsitute()
+
+    def __sub_txt(self, obj):
+        key = obj.group(1)
+        if key in self.__properties:
+            return self.__properties[key]
+        
+
+    def __eval_text(self, value):
+        pattern = re.compile(r'[$][{](.*?)[}]', re.S)
+        new_value = re.sub(pattern, self.__sub_txt, value)
+        return new_value!=value, new_value
+
+    def __recursively(self, node):
+        for e in node.childNodes:
+            if e.nodeType == minidom.Node.ELEMENT_NODE:
+                for k, v in e.attributes.items():
+                    replace , value = self.__eval_text(v)
+                    if replace:
+                        e.setAttribute(k, value)
+
+            self.__recursively(e)
+
+    def __subsitute(self):
+        self.__recursively(self.__doc)
+
+
+    def __save(self):
+        dir = os.path.dirname(__file__)
+        file_name = os.path.join(dir, "../dest_sdf")
+        file_name = os.path.join(file_name, "property.sdf")
+        with (open(file_name, "w")) as f:
+            self.__doc.writexml(f)
+
+def main():
+    dir = os.path.dirname(__file__)
+    file_name = os.path.join(dir, "../source_example")
+    file_name = os.path.join(file_name, "property.xacro")
+    con = converter(file_name)
+
+if __name__ == "__main__":
+    main()
